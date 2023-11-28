bb7cd4cf540bdc20319248ec67e5ea1e14de9d7c
shenyang <1463476251@qq.com>
2023-11-28 15:00:09 +0800

    "文章"

---
+++ src/main/java/org/example/controller/ArticleController.java
@@ -0,0 +1,36 @@
+package org.example.controller;
+
+import org.example.pojo.MyArticle;
+import org.example.service.ArticleService;
+import org.example.tools.ErrorEnum;
+import org.example.tools.JsonResultObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
+public class ArticleController {
+    @Autowired
+    ArticleService articleService;
+
+    @PostMapping("/article")
+    //防止传入空参  默认会把处理的结果传给BindingResult对象。
+    public JsonResultObject add(@Validated @RequestBody MyArticle myArticle, BindingResult bindingResult){
+        try {
+            if (bindingResult.hasErrors()){
+                return new JsonResultObject("400","新发布文章失败！",bindingResult.getFieldError().getDefaultMessage(),"Bad Params",null);
+            }
+            boolean addResult = articleService.add(myArticle);
+            if (addResult){
+                return new JsonResultObject("200","新发布文章成功","","",null);
+            }else {
+                ErrorEnum enum1 = ErrorEnum.valueOf(ErrorEnum.class,"BAD_INPUT_PARAM");
+                return new JsonResultObject("200","新发布文章失败",enum1.getErrorMsg(), enum1.getErrorCode(), null);
+            }
+        }catch (Exception e){
+            return new JsonResultObject<>("400","发布最新文章失败！", e.getMessage(), "Bad Params",null);
+        }
+    }
+}
--- src/main/java/org/example/pojo/MyArticle.java
+++ src/main/java/org/example/pojo/MyArticle.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,12 +14,17 @@ public class MyArticle {
     //文章ID
     private int id;
     //文章标题
+    //使用注解实现字段检查 ，NotBlank用于验证字符串类型不为空
+    @NotBlank(message = "文章标题不能为空")
     private String title;
     //文章作者
+    @NotBlank(message = "作者名不能是空")
     private String author;
     //文章内容
+    @NotBlank(message = "文章内容不能是空")
     private String content;
     //文章分类ID
+    @NotNull(message = "分类ID不能为空")
     private int categoryId;
     //标签 以逗号分隔
     private String tags;
